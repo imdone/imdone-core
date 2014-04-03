9141c7e4a67f3db61343b441033e004fd6b0f031
Jesse Piascik <jesse@piascik.net>
2014-04-02 21:01:41 -0500

    Project lists

--- test/project-spec.js
+++ test/project-spec.js
@@ -1,7 +1,9 @@
 var should     = require('should'), 
+    _          = require('lodash'),
     expect     = require('expect.js'),
     Repository = require('../lib/repository'),
     Project    = require('../lib/project'),
+    wrench     = require('wrench'),
     async      = require('async');
 
 var repo1 = new Repository(process.cwd() + "/test/repos/repo1");
@@ -13,20 +15,46 @@ describe("Project", function() {
       var project = new Project("Jesse", "My Project", [repo1, repo2]);
       project.init(function(err, result) {
         var lists = project.getTasks();
-        (lists.length).should.be.exactly(2);
-        (lists[0].name).should.equal('DOING');
-        (lists[0].tasks.length).should.be.exactly(6);
-        (lists[1].name).should.equal('TODO');
-        (lists[1].tasks.length).should.be.exactly(6);
+        (lists.length).should.be.exactly(3);
+        var DONE = _.find(lists, {name:"DONE"});
+        (DONE.tasks.length).should.be.exactly(1);
+        var TODO = _.find(lists, {name:"TODO"});
+        (TODO.tasks.length).should.be.exactly(6);
+        var DOING = _.find(lists, {name:"DOING"});
+        (DOING.tasks.length).should.be.exactly(6);
         done();
       });
     });
 
-    it("should return an array of lists with sorted tasks for the listName provided", function(done) {
+    it("should return an array of lists with sorted tasks for the repo provided", function(done) {
       var project = new Project("Jesse", "My Project", [repo1, repo2]);
       project.init(function(err, result) {
-        var list = project.getTasks('DOING');
-        (list.tasks.length).should.be.exactly(6);
+        var lists = project.getTasks(repo1.getId());
+        (lists.length).should.be.exactly(2);
+        var TODO = _.find(lists, {name:"TODO"});
+        (TODO.tasks.length).should.be.exactly(3);
+        var DOING = _.find(lists, {name:"DOING"});
+        (DOING.tasks.length).should.be.exactly(3);
+        done();
+      });
+    });
+  });
+
+  describe("getLists", function() {
+    it("should return the lists from a single repository", function(done) {
+      var project = new Project("Jesse", "My Project", [repo1,repo2]);
+      project.init(function(err, result) {
+        var lists = project.getLists(repo1.getId());
+        (lists.length).should.be.exactly(2);
+        done();
+      });
+    });
+
+    it("should return the lists from all repositories", function(done) {
+      var project = new Project("Jesse", "My Project", [repo1, repo2]);
+      project.init(function(err, result) {
+        var lists = project.getLists();
+        (lists.length).should.be.exactly(3);
         done();
       });
     });
--- test/repos/repo2/sample.js
+++ test/repos/repo2/sample.js
@@ -1,2 +1,3 @@
 // TODO: A js file task in repo 2
 // DOING: Just another task in repo 2
+// DONE: A done task
