895b7a2138f35d7c2dd5cfbabd0846bd1bf46eb2
Jesse Piascik <jesse@piascik.net>
2014-12-11 06:53:24 -0600

    Add missing files

---
+++ test/files/test.js
@@ -0,0 +1,12 @@
+// TODO:20 a later task in TODO
+// TODO a fixme task
+// TODO:0 a task in TODO
+// Can I DONE:60 This task now
+
+/*
+ * This is a code file #publish:10 another task
+ * TODO with a task
+ */ 
+ var dog = {
+  name:  /* [name the dog ](#publish:0)*/ "Rover"
+ };
