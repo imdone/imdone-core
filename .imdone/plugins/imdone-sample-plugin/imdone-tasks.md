0bef1e40df30f86547f89c13d7c3651f4b2abf82
Jesse <jesse@piascik.net>
2022-04-09 11:12:48 -0400

    Add card properties

--- .imdone/plugins/imdone-sample-plugin/imdone-tasks.md
+++ .imdone/plugins/imdone-sample-plugin/imdone-tasks.md
@@ -4,5 +4,4 @@
 |------------------------|---------------------------------------------|------------------------------------------------|
 | **What's Due?**        | <!--[{{totals["What's Due?"]}}]-->          | <!--[ {{dueEmoji}} ]-->                        |
 | **Recently Completed** | <!--[ {{totals["Recently Completed"]}} ]--> | <!--[ {{recentEmoji}} ]-->                     |
-| **WIP**                | <!--[ {{totals["DOING"]}} ]-->              | <!--[ {{wipEmoji}} ]-->                        |
-<!-- expand::1 refresh::1 metadata::value #three epic::"Release 1.25" epic::"Release 1.26" -->
+| **WIP**                | <!--[ {{totals["DOING"]}} ]-->              | <!--[ {{wipEmoji}} ] expand:1 -->                        |
---
+++ .imdone/properties/card.js
@@ -0,0 +1,72 @@
+module.exports = function ({ line, source, totals, allMeta }) {
+  const release =
+    allMeta.epic &&
+    allMeta.epic[0].includes('Release') &&
+    allMeta.epic[0].split(' ').pop()
+  return {
+    hashtags:
+      '#100daysofcode #programmer #developer #hacking #markdown #Productivity #indiehackers #blogger',
+    date: `${new Date().toISOString().substring(0, 10)}`,
+    timestamp: `${new Date().toISOString()}`,
+    sourceLink: `[${source.path}:${line}](${source.path}:${line})`,
+    dueEmoji: dueEmoji(totals),
+    recentEmoji: recentEmoji(totals),
+    wipEmoji: wipEmoji(totals),
+    cardTotal: cardTotal(totals),
+    release,
+  }
+}
+
+const EMOJI = {
+  BAD: ':rotating_light:',
+  GREAT: ':rocket:',
+  SLEEP: ':sleeping:',
+  GOOD: ':2nd_place_medal:',
+}
+
+function formatEmoji(emoji) {
+  return `<span style="font-size: 1.5em;">${emoji}</span>`
+}
+
+function dueEmoji(totals) {
+  const due = totals["What's Due?"]
+  let emoji = EMOJI.GOOD
+  if (due >= 3) {
+    emoji = EMOJI.BAD
+  } else if (due === 0) {
+    emoji = EMOJI.GREAT
+  }
+  return formatEmoji(emoji)
+}
+
+function recentEmoji(totals) {
+  const recentlyCompleted = totals['Recently Completed']
+  let emoji = EMOJI.GOOD
+  if (recentlyCompleted >= 3) {
+    emoji = EMOJI.GREAT
+  } else if (recentlyCompleted === 0) {
+    emoji = EMOJI.BAD
+  }
+  return formatEmoji(emoji)
+}
+
+function wipEmoji(totals) {
+  const doing = totals['DOING']
+  let emoji = EMOJI.GOOD
+  if (doing >= 3) {
+    emoji = EMOJI.BAD
+  } else if (doing === 0) {
+    emoji = EMOJI.SLEEP
+  } else if (doing === 1) {
+    emoji = EMOJI.GREAT
+  }
+  return formatEmoji(emoji)
+}
+
+function cardTotal(totals) {
+  let count = 0
+  Object.keys(totals).forEach((list) => {
+    count += totals[list]
+  })
+  return count
+}
