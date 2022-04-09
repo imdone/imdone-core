0f58d682340a893a32f124b8670fd804c804d6f0
Jesse <jesse@piascik.net>
2022-04-09 14:19:45 -0400

    Switch to master

--- .imdone/actions/board.js
+++ .imdone/actions/board.js
@@ -1,10 +1,24 @@
 const release = require('./lib/release')
+const path = require('path')
+const git = require('isomorphic-git')
+const http = require('isomorphic-git/http/node')
+const fs = require('fs')
 
 module.exports = function () {
   const project = this.project
   const { getChangeLog } = release(project)
 
   return [
+    {
+      title: 'Start release',
+      action: async function () {
+        await git.checkout({
+          fs,
+          dir: project.path,
+          ref: 'master',
+        })
+      },
+    },
     {
       title: 'Copy changelog',
       action: function () {
--- .imdone/actions/card.js
+++ .imdone/actions/card.js
@@ -1,4 +1,36 @@
+const eol = require('eol')
+
 module.exports = function (task) {
   const project = this.project
-  return []
+  return [
+    {
+      title: `Copy card for code`,
+      action: () => {
+        const prefix = '// '
+        const sep = eol.auto
+        const contentLines = task.content.split(sep)
+
+        contentLines[0] = `## ${contentLines[0]}`
+        contentLines.splice(
+          1,
+          0,
+          '**code:** [${relPath}:${line}](${relPath}:${line})'
+        )
+
+        if (contentLines[contentLines.length - 1].trim() === '')
+          contentLines.pop()
+
+        const content = `${prefix}${task.list}:${
+          task.order
+        } ${contentLines.join(`${sep}${prefix}`)}`
+
+        return project.copyToClipboard(
+          content,
+          `Card for code copied to clipboard`
+        )
+      },
+      pack: 'fas',
+      icon: 'clone',
+    },
+  ]
 }
