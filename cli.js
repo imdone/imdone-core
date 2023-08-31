499f21a8947c34bc0c14cd06c449210f188de5b5
Jesse Piascik <jesse@piascik.net>
2023-08-31 00:49:54 -0400

    Add main cli commands

--- cli.js
+++ cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const { program } = require('commander');
-const _path = require('path')
+const { resolve } = require('path')
 const { createFileSystemProject } = require('./lib/project-factory')
-const { loadYAML } = require('./tools')
+const { loadYAML } = require('./lib/tools')
 const { readFile } = require('fs/promises')
 const Config = require('./lib/config')
 
@@ -12,39 +12,73 @@ newConfigFromFile = async (configPath) => {
   return new Config(loadYAML(config))
 }
 
+const log = console.log
+const info = console.info
+const warn = console.warn
+const logQueue = {warn: [], info: [], log: []}
+Object.keys(logQueue).forEach((key) => {
+  console[key] = function(...args) {
+    logQueue[key].push(args)
+  }
+})
 
-program.command('init')
+program
+.command('init')
 .description('Initialize imdone project')
-.option('-p, --path <path>', 'The path to the imdone project')
-.option('-c, --config <config>', 'The path to the imdone config file')
+.option('-p, --project-path <path>', 'The path to the imdone project')
+.option('-c, --config-path <path>', 'The path to the imdone config file')
 .action(async function () {
-  let { path = process.env.PWD, config } = this.opts()
-  path = _path.resolve(path)
-  if (config) {
-    configPath = _path.resolve(config)
+  let { projectPath = process.env.PWD, configPath } = this.opts()
+  projectPath = resolve(projectPath)
+  let config
+  if (configPath) {
+    configPath = resolve(configPath)
     config = await newConfigFromFile(configPath)
   }
-  const project = createFileSystemProject({path, config})
+  const project = createFileSystemProject({path: projectPath, config})
   await project.init()
 })
 
 program
 .command('add <task>')
 .description('Add a task')
-.option('-p, --path <path>', 'The path to the imdone project')
+.option('-p, --project-path <path>', 'The path to the imdone project')
 .option('-l, --list <list>', 'The task list to use')
-.action(function () {
-  console.log(this.args)
-  console.log(this.opts())
+.action(async function () {
+  let { projectPath = process.env.PWD, list } = this.opts()
+  projectPath = resolve(projectPath)
+  const project = createFileSystemProject({path: projectPath})
+  await project.init()
+  const data = await project.addTaskToFile({list, content: this.args[0]})
 })
 
-program.command('ls')
+program
+.command('ls')
 .description('List tasks')
-.option('-p, --path <path>', 'The path to the imdone project')
-.option('-l, --list <list>', 'The task list to use')
-.option('-f, --filter <filter>', 'Filter tasks')
-.action(function () {
-  console.log(this.args)
-  console.log(this.opts())
+.option('-p, --project-path <path>', 'The path to the imdone project')
+.option('-f, --filter <filter>', 'The filter to use')
+.option('-j, --json', 'Output as json')
+.action(async function () {
+  let { projectPath = process.env.PWD, filter, json } = this.opts()
+  projectPath = resolve(projectPath)
+  const project = createFileSystemProject({path: projectPath})
+  await project.init()
+  const tasks = project.getCards(filter)
+  const lists = project.getLists({tasks})
+
+  if (json) return log(JSON.stringify(lists, null, 2))
+
+  lists.forEach((list) => {
+    const tasks = list.tasks
+    if (tasks.length > 0) {
+      log('')
+      log(list.name)
+      log('====================\n')
+      tasks.forEach((task) => {
+        log(`- ${task.text}`)
+      })
+      log('')
+    }
+  })
 })
 program.parse();
