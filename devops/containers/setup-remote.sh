c92e0d58cfee63e786072c4178652df572831e15
Jesse Piascik <jesse@piascik.net>
2023-11-12 21:45:32 -0500

    Run container setup in test

--- devops/containers/setup-remote.sh
+++ devops/containers/setup-remote.sh
@@ -12,7 +12,7 @@ docker run \
   --volume git-remote.host-keys:/etc/ssh/host_keys \
   1nfiniteloop/git-remote:latest
 
-docker exec -it git-remote mkdir /home/git/authorized_keys
+docker exec git-remote mkdir /home/git/authorized_keys
 
 # TODO: Generate a key in the container and use that instead of copying the key from the host
 # Update to use `GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa_example" git clone example`
@@ -21,8 +21,8 @@ docker exec -it git-remote mkdir /home/git/authorized_keys
 docker cp ~/.ssh/id_rsa.pub git-remote:/home/git/authorized_keys
 
 echo "make authorized_keys owned by git"
-docker exec -it git-remote chown -R git:git /home/git/authorized_keys/id_rsa.pub
-docker exec -it git-remote chmod 777 /home/git/authorized_keys/id_rsa.pub
+docker exec git-remote chown -R git:git /home/git/authorized_keys/id_rsa.pub
+docker exec git-remote chmod 777 /home/git/authorized_keys/id_rsa.pub
 
 echo "create the test project"
-docker exec -it git-remote su - git -s /bin/bash -c "mkdir /home/git/test-project.git; cd /home/git/test-project.git; git init --bare;"
+docker exec git-remote su - git -s /bin/bash -c "mkdir /home/git/test-project.git; cd /home/git/test-project.git; git init --bare;"
