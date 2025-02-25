b7d1a1f093c8848d15df991ec4aa8f439b76dc02
Jesse Piascik <jesse@piascik.net>
2025-02-25 11:29:01 -0500

    Fix restoring hidden lists

--- test/repository-spec.js
+++ test/repository-spec.js
@@ -431,7 +431,7 @@ describe('Repository', function () {
       await proj1.init()
       const bareList = {name: "Filtered", filter: "text = /task/", id: "filtered"}     
       const filtered = new List(bareList)
-      repo1.addList(filtered)
+      await repo1.addList(filtered)
       const { lists } = await proj1.toImdoneJSON()
       expect(lists.find(list => list.id === filtered.id).tasks.length).to.be(6)
       await repo1.updateList(filtered.id, {...bareList, filter: "text = /tasks/"})
