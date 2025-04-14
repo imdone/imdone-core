a2f868d90cb96feb6bd3ebbc040007247efeb081
Jesse Piascik <jesse@piascik.net>
2025-04-14 14:43:54 -0400

    Update config type

--- types/adapters/storage/config.d.ts
+++ types/adapters/storage/config.d.ts
@@ -2,5 +2,6 @@ export function load(projectPath: any, configPath?: string): Promise<Config | un
 export function save(config: any, projectPath: any): Promise<void>;
 export function loadForFilePath(filePath: any): Promise<Config | undefined>;
 export function loadYamlConfig(filePath: any): Promise<{} | undefined>;
+export function isImdoneProject(projectPath: any): Promise<boolean>;
 export function findImdonePath(cwd: any): Promise<string>;
 import { Config } from '../../config.js';
