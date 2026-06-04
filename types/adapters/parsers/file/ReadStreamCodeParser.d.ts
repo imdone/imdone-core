0690d8d0ad2cd8655aeaa1183587c63003213816
Jesse Piascik <jpiascik@flexion.us>
2026-06-04 13:57:06 -0400

    SCRUM-347: parse code tasks incrementally from read streams
    Replace ReadStreamCodeParser full-file buffering with incremental comment parsing.
    
    - Preserve block comment task discovery
    - Add adjacent single-line comment coverage
    - Regenerate parser types

--- types/adapters/parsers/file/ReadStreamCodeParser.d.ts
+++ types/adapters/parsers/file/ReadStreamCodeParser.d.ts
@@ -3,11 +3,9 @@ export class ReadStreamCodeParser extends ReadStreamTaskParser {
     lists: any;
     filePath: any;
     lang: any;
-    taskParsers: CodeStyleTaskParser[];
     init(): Promise<void>;
     cards: any[] | undefined;
-    parseCards(code: any, tokens?: string[]): any[];
-    parseComments(code: any): any;
+    parseCards(tokens?: string[]): Promise<any[]>;
+    parseComments(): Promise<{}>;
 }
 import { ReadStreamTaskParser } from './ReadStreamTaskParser.js';
-import { CodeStyleTaskParser } from '../task/CodeStyleTaskParser.js';
